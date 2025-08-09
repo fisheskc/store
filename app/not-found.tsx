e35da36a9d7f1cf421869711f174599f20668ef1
Karen Collins <fisheskc@googlemail.com>
2025-08-09 15:22:10 +0100

    Updated the not fount page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,9 @@
-import Link from 'next/link'
+import Link from "next/link"
+
 export default function NotFound() {
  return (
     <div>
-      <Link href='/'>Return Home</Link>
+      <Link href="/">Return Home</Link>
     </div>
  )
 }
